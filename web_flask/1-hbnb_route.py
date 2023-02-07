7858a866b480839a4f058878d2a22869632731a0
theeduke <oscarian528@gmail.com>
2023-02-07 13:02:22 -0800

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,5 +17,11 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB' . """
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0, port=5000")
