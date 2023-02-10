9ec0daa942500aee7b07e00e613596fb6bac3be1
theeduke <oscarian528@gmail.com>
2023-02-10 03:46:52 -0800

    task successful

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,33 @@
-#!/usr/bin/python 3
+#!/usr/bin/python3
+""" starts a Flask web application:
+Your web application must be listening on 0.0.0.0, port 5000
+Routes: /: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition
 """
-    web application to listen on 0.0.0.0, port 5000
-    option "strict_slashes=False"
-    routes
-        /:display"Hello HBNB"
-        /hbnb : display "HBNB"
-        /c/<text>: Displays 'C' followed by the value of the <text>
-"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+def hello():
+    """returning  Hello HBNB str"""
     return ("Hello HBNB!")
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """returning  HBNB str"""
     return ("HBNB")
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>.
-    text = text.replace("_", " ")
+def c_text(text):
+    """display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space)
     """
-    return ("C {}".format(text))
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
